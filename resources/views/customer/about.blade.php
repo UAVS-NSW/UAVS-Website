bd58cf475de3fa3b2a58fff3431989d0dcbf738c
PierceLe <haichau.hvsg@gmail.com>
2025-03-01 03:47:04 +1100

    add: visuss and western

--- resources/views/customer/about.blade.php
+++ resources/views/customer/about.blade.php
@@ -49,6 +49,26 @@
                         {{--                        ---------------------------------------}}
                         <!-- Fixed Partner 1 -->
                         <div class="partner-item">
+                            <div class="item-image" style="background-image: url('assets/images/western.png');"> </div>
+                            <div class="item-description">
+                                <h3>Vietnamese International Student Association at WSU </h3>
+                                <p><i class="fas fa-envelope"></i> : western.visa@student.westernsydney.edu.au</p>
+{{--                                <p><i class="fas fa-globe"></i> : <a href="https://www.sydney.edu.au" target="_blank">sydney.edu.au</a></p>--}}
+                                <p><i class="fas fa-address-card"></i> : Sydney, NSW, Australia</p>
+                            </div>
+					    </div>
+
+                        <div class="partner-item">
+                            <div class="item-image" style="background-image: url('assets/images/visuss.png');"> </div>
+                            <div class="item-description">
+                                <h3>Vietnamese International Students at the University of Sydney</h3>
+                                <p><i class="fas fa-envelope"></i> : info.vsus@gmail.com</p>
+{{--                                <p><i class="fas fa-globe"></i> : <a href="https://www.sydney.edu.au" target="_blank">sydney.edu.au</a></p>--}}
+                                <p><i class="fas fa-address-card"></i> : Sydney, NSW, Australia</p>
+                            </div>
+                        </div>
+
+                        <div class="partner-item">
                             <div class="item-image" style="background-image: url('assets/images/logo.png');"> </div>
                             <div class="item-description">
                                 <h3>University of Sydney</h3>
@@ -56,7 +76,17 @@
                                 <p><i class="fas fa-globe"></i> : <a href="https://www.sydney.edu.au" target="_blank">sydney.edu.au</a></p>
                                 <p><i class="fas fa-address-card"></i> : Sydney, NSW, Australia</p>
                             </div>
-					    </div>
+                        </div>
+
+                        <div class="partner-item">
+                            <div class="item-image" style="background-image: url('assets/images/logo.png');"> </div>
+                            <div class="item-description">
+                                <h3>University of Sydney</h3>
+                                <p><i class="fas fa-envelope"></i> : contact@sydney.edu.au</p>
+                                <p><i class="fas fa-globe"></i> : <a href="https://www.sydney.edu.au" target="_blank">sydney.edu.au</a></p>
+                                <p><i class="fas fa-address-card"></i> : Sydney, NSW, Australia</p>
+                            </div>
+                        </div>
 
                         <div class="partner-item">
                             <div class="item-image" style="background-image: url('assets/images/logo.png');"> </div>
