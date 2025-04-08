25f35b55b3ceb37d2fc903f78d2cb56a6acf74c1
quanghung304 <caohung304@gmail.com>
2025-04-09 02:58:15 +0700

    Increase memory_limit to 256MB

--- public/check_config.php
+++ public/check_config.php
@@ -1,3 +1,4 @@
 <?php
 echo "Upload Max Filesize: " . ini_get('upload_max_filesize') . "<br>";
 echo "Post Max Size: " . ini_get('post_max_size') . "<br>";
+echo "Memory Limit: " . ini_get('memory_limit') . "<br>";
